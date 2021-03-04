acd034d9e30aca95300f2d4577f9c255685c85ae
Daniel Gallagher <daniel.gallagher@parks.nyc.gov>
2021-03-03 21:57:11 -0500

    Updated the merge script with mostly finalized schema. Related to #9.

--- sp_m_gis_pip.sql
+++ sp_m_gis_pip.sql
@@ -26,20 +26,41 @@ create procedure dbo.sp_m_gis_pip as
 		
 		with gis_allsites as(
 			select *
-			from [gisdata].parksgis.dpr.vw_pip_compatible_inspected_sites
+			from openquery([gisdata], 'select * from parksgis.dpr.vw_pip_compatible_inspected_sites')
 			/*Probably need to build in duplicate and null exclusion here*/)
 
 		merge accessnewpip.dbo.tbl_ref_allsites as tgt using gis_allsites as src
-		on (tgt.[prop id] = src.[prop id])
-		/*If the records are matched based on the identifiers, but the record in GIS has a greater edit date than the record in PIP
-		  then update the whole record.*/
-		when matched and tgt.last_edited_date < src.last_edited_date
-			then update set col = src.col
+		/*Use the omppropid aka prop id as the merge key*/
+		on (tgt.[prop id] != src.[prop id])
+		/*If the records are matched based on the identifiers, but the row hashes are different then perform an update*/
+		when matched and tgt.row_hash != src.row_hash
+			then update set propnum = src.propnum,
+							boro = src.boro,
+							ampsdistrict = src.ampsdistrict,
+							[prop name] = src.[prop name],
+							[site name] = src.[site name],
+							[prop location] = src.[prop location],
+							[site location] = src.[site location],
+							jurisdiction = src. jurisdiction,
+							typecategory = src.typecategory,
+							acres = src.acres,
+							gisobjid = src.gisobjid,
+							sourcefc = src.sourcefc
+		/*If the record is in GIS, but not in PIP then perform an insert.*/
 		when not matched by target
-			then insert(col)
-				values(src.col)
+			then insert(propnum, 
+						boro, 
+						ampsdistrict,
+						[prop name],
+						[site name],
+						[prop location],
+						[site location],
+						jurisdiction,
+						typecategory,
+						acres,
+						gisobjid,
+						sourcefc)
+				values(src.propnum, src.boro, src.ampsdistrict, src.[prop name], src.[site name], src.[prop location], src.[site location],
+					   src.jurisdiction, src.typecategory, src.acres, src.gisobjid, src.sourcefc)
 		/*Skip the delete section because we can't break any keys and the cascade effect is not known.*/
 	commit;
-
-	select *
-	from sys.servers
