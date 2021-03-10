36d9453280e11262b0bff03bba56e5bd6878852d
Daniel Gallagher <daniel.gallagher@parks.nyc.gov>
2021-03-09 20:58:30 -0500

    Added trigger to carry inserted records (prop id) from tbl_ref_allsites to tbl_pip_allsites, closes #25.

---
+++ trg_fi_tbl_ref_allsites.sql
@@ -0,0 +1,30 @@
+/***********************************************************************************************************************
+																													   	
+ Created By: Dan Gallagher, daniel.gallagher@parks.nyc.gov, Innovation & Performance Management         											   
+ Modified By: <Modifier Name>																						   			          
+ Created Date:  <MM/DD/YYYY>																							   
+ Modified Date: <MM/DD/YYYY>																							   
+											       																	   
+ Project: <Project Name>	
+ 																							   
+ Tables Used: <Database>.<Schema>.<Table Name1>																							   
+ 			  <Database>.<Schema>.<Table Name2>																								   
+ 			  <Database>.<Schema>.<Table Name3>				
+			  																				   
+ Description: <Lorem ipsum dolor sit amet, legimus molestiae philosophia ex cum, omnium voluptua evertitur nec ea.     
+	       Ut has tota ullamcorper, vis at aeque omnium. Est sint purto at, verear inimicus at has. Ad sed dicat       
+	       iudicabit. Has ut eros tation theophrastus, et eam natum vocent detracto, purto impedit appellantur te	   
+	       vis. His ad sonet probatus torquatos, ut vim tempor vidisse deleniti.>  									   
+																													   												
+***********************************************************************************************************************/
+use accessnewpip
+go
+
+create trigger dbo.trg_fi_tbl_ref_allsites
+on accessnewpip.dbo.tbl_ref_allsites
+for insert as
+	begin transaction
+		insert into accessnewpip.dbo.tbl_pip_allsites([prop id])
+			select [prop id]
+			from inserted
+	commit;
