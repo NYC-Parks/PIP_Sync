2b5eaaa318997d7fb65e601d64c8e96b17efc685
Daniel Gallagher <daniel.gallagher@parks.nyc.gov>
2021-03-25 14:09:10 -0400

    Created snapshot of allsites and saved excel to O365. Closes #48.

---
+++ allsites_snapshot.sql
@@ -0,0 +1,22 @@
+/***********************************************************************************************************************
+																													   	
+ Created By: Dan Gallagher, daniel.gallagher@parks.nyc.gov, Innovation & Performance Management         											   
+ Modified By: <Modifier Name>																						   			          
+ Created Date:  <MM/DD/YYYY>																							   
+ Modified Date: <MM/DD/YYYY>																							   
+											       																	   
+ Project: <Project Name>	
+ 																							   
+ Tables Used: <Database>.<Schema>.<Table Name1>																							   
+ 			  <Database>.<Schema>.<Table Name2>																								   
+ 			  <Database>.<Schema>.<Table Name3>				
+			  																				   
+ Description: <Lorem ipsum dolor sit amet, legimus molestiae philosophia ex cum, omnium voluptua evertitur nec ea.     
+	       Ut has tota ullamcorper, vis at aeque omnium. Est sint purto at, verear inimicus at has. Ad sed dicat       
+	       iudicabit. Has ut eros tation theophrastus, et eam natum vocent detracto, purto impedit appellantur te	   
+	       vis. His ad sonet probatus torquatos, ut vim tempor vidisse deleniti.>  									   
+																													   												
+***********************************************************************************************************************/
+select *
+into accessnewpip.dbo.allsites_snapshot_20210325
+from accessnewpip.dbo.allsites
